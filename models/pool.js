387d2dc29ab3cd06f77dc0275bf0583f13b87db3
Mohammed Akram <nerddude9000@proton.me>
2026-08-05 18:11:26 +0100

    config: make it possible to use connectionString

--- models/pool.js
+++ models/pool.js
@@ -3,10 +3,14 @@ const path = require("path");
 
 require("process").loadEnvFile(path.join(__dirname, "../.env"));
 
-module.exports = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  database: process.env.DB,
-  password: process.env.PSWD,
-  port: process.env.DB_PORT,
-});
+module.exports = new Pool(
+  process.env.CONNECTION_STRING
+    ? { connectionString: process.env.CONNECTION_STRING }
+    : {
+        host: process.env.HOST,
+        user: process.env.USER,
+        database: process.env.DB,
+        password: process.env.PSWD,
+        port: process.env.DB_PORT,
+      },
+);
